60e61eb19ee60f7ce82f65b0f415728d1aeaf656
sonu-james <sonupjames@gmail.com>
2025-10-10 20:21:11 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   base: '/TBS_Solutions-WhiteMatrix/', // your repo name here
   plugins: [react()],
 })
